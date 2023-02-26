37d74ddd8aaa1fad241b6f14af662deac942e57f
kendall-janka <126126288+kendall-janka@users.noreply.github.com>
2023-02-26 16:50:40 -0500

    Call API and change Temp on Inner HTML

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
-<script
-  src="https://kit.fontawesome.com/d43a10294f.js"
-  crossorigin="anonymous"
-></script>;
+let apiKey = "4900o839bet5aaf3e9a5c07fb3d6686a";
+let apiUrl =
+  "https://api.shecodes.io/weather/v1/current?query=Lisbon&key=4900o839bet5aaf3e9a5c07fb3d6686a&units=imperial";
+console.log(apiUrl);
+axios.get(apiUrl).then(displayTemperature);
+
+function displayTemperature(response) {
+  console.log(response.data.temperature.current);
+  let temperatureElement = document.querySelector("#currentTemperature");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+}
