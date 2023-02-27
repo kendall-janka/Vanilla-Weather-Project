803fe820a005446613a52f42b25009d8a20494bc
kendall-janka <126126288+kendall-janka@users.noreply.github.com>
2023-02-26 19:21:00 -0500

    Format Date and Time

--- src/index.js
+++ src/index.js
@@ -23,6 +23,36 @@ function displayTemperature(response) {
   windSpeedElement.innerHTML = Math.round(response.data.wind.speed);
   console.log(response.data.wind.speed);
 
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDate(response.data.time * 1000);
+
   //let currentIconElement = document.querySelector("#currentIcon");
   //currentIconElement.innerHTML = response.data.condition.icon_url;
+}
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  //if (hours>12){
+  //hours = ${hours}-12;
+  // }
+
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  return `${day}${hours}:${minutes}`;
 }
