46f2cffe7d1eafac7e6cdae3865a2ecba1b13dfa
kendall-janka <126126288+kendall-janka@users.noreply.github.com>
2023-03-03 08:24:36 -0500

    Edit day display for consistency

--- src/index.js
+++ src/index.js
@@ -68,7 +68,7 @@ function formatDate(timestamp) {
 function formatDay(timestamp) {
   let date = new Date(timestamp * 1000);
   let day = date.getDay();
-  let days = ["Sun", "Mon", "Tues", "Wed", "Thu", "Fri", "Sat"];
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
   return days[day];
 }
 
